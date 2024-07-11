a88d1769c8fae8cdc245654a43e5ff38c40c996a
Valentin Thirion <valentinthirion@gmail.com>
2024-07-11 11:01:32 +0200

    [ADD] Added base config script

---
+++ new_config.sh
@@ -0,0 +1,12 @@
+# Script to create a new odoo configuration structure
+
+# TODO : continue the script
+
+# variable to store the config name
+config_name = "customername"
+
+# odoo version number
+odoo_version = "17.0"
+postgres_version = "14"
+
+# create a new
